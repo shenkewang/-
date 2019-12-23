05109812b2d0467f288fa2476da6cc6a4618eeb0
shenkewang <1226698825@qq.com>
2019-12-23 12:00:06 +0800

    商品1

--- src/components/shangpin/shangpin.vue
+++ src/components/shangpin/shangpin.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="shangpins">
-    <div class="zuos">
+    <div class="zuos" ref="zuogundong">
       <ul class="daohangs">
         <li class="daohang" v-for="(item,index) in shuju" :key="index">
           <span class="text border-1px">
@@ -10,36 +10,37 @@
         </li>
       </ul>
     </div>
-    <div class="yous">
-        <ul class="you">
-          <li class="you-item" v-for="(items,index) in shuju" :key="index">
-            <h1 class="you-item-text">{{items.name}}</h1>
-            <ul class="you-item-items">
-              <li class="you-item-item" v-for="(ite,idx) in items" :key="idx">
-                <div class="tupian">
-                  <img width="57" height="57" :src="ite.icon" alt="" >
+    <div class="yous" ref="yougundong">
+      <ul class="you">
+        <li class="you-item" v-for="(items,index) in shuju" :key="index">
+          <h1 class="you-item-text">{{items.name}}</h1>
+          <ul class="you-item-items" v-for="(itemx,inx) in items.foods" :key="inx">
+            <li class="you-item-item border-1px">
+              <div class="tupian">
+                <img width="57" height="57" :src="itemx.icon" alt />
+              </div>
+              <div class="text">
+                <h2 class="biaoti">{{itemx.name}}</h2>
+                <div class="miaoshu">{{itemx.description}}</div>
+                <div class="yueshous">
+                  <span class="yueshou">月售{{itemx.sellCoun}}份</span>
+                  <span>好评率{{itemx.rating}}%</span>
                 </div>
-                <div class="text">
-                  <h2 class="biaoti">{{items.name}}</h2>
-                  <div class="miaoshu">{{ite.description}}</div>
-                  <div class="yueshous">
-                    <span class="yueshou">月售{{ite.sellCount}}分</span>
-                    <span>好评率{{ite.rating}}</span>
-                  </div>
-                  <div class="jiage">
-                    <span class="xianjia">￥{{ite.price}}</span>
-                    <span v-if="ite.oldPrice" class="yuanjia">￥{{ite.oldPrice}}</span>
-                  </div>
+                <div class="jiage">
+                  <span class="xianjia">￥{{itemx.price}}</span>
+                  <span v-if="itemx.oldPrice" class="yuanjia">￥{{itemx.oldPrice}}</span>
                 </div>
-              </li>
-            </ul>
-          </li>
-        </ul>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 <script>
 import shangpin from "@/qingqiushuju/toubushuju.js";
+import Bscroll from "@better-scroll/core";
 export default {
   data() {
     return {
@@ -47,8 +48,15 @@ export default {
     };
   },
   async created() {
-    this.classMap=["decrease", "discount", "guarantee", "invoice", "special"];
+    this.classMap = ["decrease", "discount", "guarantee", "invoice", "special"];
     this.shuju = await shangpin("/api/goods");
+    this.initScroll();
+  },
+  methods:{
+    initScroll(){
+      this.zuoScroll=new Bscroll(this.$refs.zuogundong);
+      this.youScroll=new Bscroll(this.$refs.yougundong);
+    }
   }
 };
 </script>
@@ -65,6 +73,9 @@ export default {
     flex: 0 0 0.8rem;
     width: 0.8rem;
     .daohangs {
+      &:last-child {
+        @include border-none;
+      }
       .daohang {
         display: table;
         padding: 0 0.12rem;
@@ -86,87 +97,90 @@ export default {
         vertical-align: top;
         width: 0.12rem;
         height: 0.12rem;
-            &.decrease {
-            @include bg-image("./img/decrease_3");
-            }
-            &.discount {
-              @include bg-image("./img/discount_3");
-            }
-            &.guarantee {
-              @include bg-image("./img/guarantee_3");
-            }
-            &.invoice {
-              @include bg-image("./img/invoice_3");
-            }
-            &.special {
-              @include bg-image("./img/special_3");
-            }
+        &.decrease {
+          @include bg-image("./img/decrease_3");
+        }
+        &.discount {
+          @include bg-image("./img/discount_3");
+        }
+        &.guarantee {
+          @include bg-image("./img/guarantee_3");
+        }
+        &.invoice {
+          @include bg-image("./img/invoice_3");
+        }
+        &.special {
+          @include bg-image("./img/special_3");
+        }
       }
     }
   }
   .yous {
-    li{
-      list-style:none;
+    li {
+      list-style: none;
     }
     flex: 1;
-    .you-item-text{
-      height:.26rem;
-      padding-left:.14rem;
-      line-height:.26rem;
-      border-left:2px solid #d9dde1;
-      font-size:.12rem;
-      color:rgb(143,157,159);
-      background-color:#f3f5f7;
+    .you-item-text {
+      height: 0.26rem;
+      padding-left: 0.14rem;
+      line-height: 0.26rem;
+      border-left: 2px solid #d9dde1;
+      font-size: 0.12rem;
+      color: rgb(143, 157, 159);
+      background-color: #f3f5f7;
     }
-    .you-item-items{
-      .you-item-item{
-        margin:.18rem;
-        display:flex;
-        @include border-1px(rgba(7,17,27,0.1));
-        .tupian{
-          width:.6rem;
-          height:.6rem;
-          margin-right:.1rem;
-          img{   
-          vertical-align: top;   
+    .you-item-items {
+      &:last-child {
+        @include border-none;
+      }
+      .you-item-item {
+        margin: 0.18rem;
+        display: flex;
+        @include border-1px(rgba(7, 17, 27, 0.1));
+
+        .tupian {
+          width: 0.6rem;
+          height: 0.6rem;
+          margin-right: 0.1rem;
+          img {
+            vertical-align: top;
           }
         }
-        .text{
-
-          margin-top:.02rem;
-          .biaoti{
-            font-size:.14rem;
-            color:rgb(7,17,27);
-            line-height:.14rem;
-
+        .text {
+          margin-top: 0.02rem;
+          .biaoti {
+            font-size: 0.14rem;
+            color: rgb(7, 17, 27);
+            line-height: 0.14rem;
           }
-          .miaoshu{
-            font-size:.1rem;
-            color: rgb(143,157,159);
-            line-height:.1rem;
-            margin:.08rem 0;
+          .miaoshu {
+            font-size: 0.1rem;
+            color: rgb(143, 157, 159);
+            line-height: 0.1rem;
+            margin: 0.08rem 0;
           }
-          .yueshous{
-            font-size:.1rem;
-            color: rgb(143,157,159);
-            line-height:.1rem;
-            .yueshou{
-              margin-right: .14rem;
+          .yueshous {
+            font-size: 0.1rem;
+            color: rgb(143, 157, 159);
+            line-height: 0.1rem;
+            .yueshou {
+              margin-right: 0.14rem;
             }
           }
-          .jiage{
-            line-height:.24rem;
-            font-weight:700;
-            font-size: .2rem;
-            .xianjia{
-              font-size:.14em;
-              color:rgb(240,20,20);
+          .jiage {
+            line-height: 0.24rem;
+            font-weight: 700;
+            font-size: 0.2rem;
+            margin-bottom: 0.16rem;
+            .xianjia {
+              font-size: 0.14em;
+              color: rgb(240, 20, 20);
             }
-            .yuanjia{
-              margin-left:.08rem;
-              font-size:.1rem;
-              color:rgb(143,157,159);
-              text-decoration:line-through;
+            .yuanjia {
+              margin-left: 0.08rem;
+              font-size: 0.1rem;
+              color: rgb(143, 157, 159);
+              text-decoration: line-through;
             }
           }
         }
